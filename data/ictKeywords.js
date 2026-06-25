d47a45d7609ee46b98f783720061c185e5df810a
mlozeman-creator <m.lozeman@gmail.com>
2026-06-26 00:29:18 +0200

    Add temporary ICT keywords for AI Assistant
    Added temporary ICT keywords for AI Assistant.

--- data/ictKeywords.js
+++ data/ictKeywords.js
@@ -1,1 +1,97 @@
+/*
+========================================
+ICT AI Assistant
+Temporary ICT Keywords
+Version 1.3.1
 
+NOTE:
+This file is temporary.
+
+It will be replaced in a future release
+by the AI Scope Engine that classifies
+questions using AI instead of keywords.
+========================================
+*/
+
+const ictKeywords = [
+
+    // General ICT
+
+    "ict",
+    "computer",
+    "software",
+    "hardware",
+    "informatica",
+    "digitalisering",
+    "technologie",
+    "ai",
+
+    // Programming
+
+    "programmeren",
+    "code",
+    "html",
+    "css",
+    "javascript",
+    "js",
+    "typescript",
+    "python",
+    "java",
+    "c#",
+    "php",
+    "sql",
+    "json",
+    "xml",
+
+    // Web
+
+    "api",
+    "rest",
+    "graphql",
+    "frontend",
+    "backend",
+    "fullstack",
+
+    // Databases
+
+    "database",
+    "mysql",
+    "postgresql",
+    "mongodb",
+
+    // Networking
+
+    "ip",
+    "ip-adres",
+    "subnet",
+    "subnetmasker",
+    "router",
+    "switch",
+    "gateway",
+    "dns",
+    "dhcp",
+    "tcp",
+    "udp",
+    "vpn",
+    "firewall",
+
+    // Operating Systems
+
+    "windows",
+    "linux",
+    "macos",
+
+    // Cloud
+
+    "azure",
+    "aws",
+    "cloud",
+    "docker",
+    "kubernetes",
+
+    // Version Control
+
+    "git",
+    "github"
+
+];
