91a08042f866f096b9c90fcf49e8fda182291ec3
mlozeman-creator <m.lozeman@gmail.com>
2026-06-26 00:32:12 +0200

    Update promptBuilder.js

--- modules/promptBuilder.js
+++ modules/promptBuilder.js
@@ -1,1 +1,50 @@
+/*
+========================================
+ICT AI Assistant
+Prompt Builder
+Version 1.3.1
+========================================
+*/
 
+function buildPrompt(question, responseStyle) {
+
+    if (!question) {
+
+        return "";
+
+    }
+
+    let template =
+        PromptTemplates.standard;
+
+    switch (responseStyle) {
+
+        case "compact":
+
+            template =
+                PromptTemplates.compact;
+
+            break;
+
+        case "extensive":
+
+            template =
+                PromptTemplates.extensive;
+
+            break;
+
+        default:
+
+            template =
+                PromptTemplates.standard;
+
+            break;
+
+    }
+
+    return template.replace(
+        "{question}",
+        question
+    );
+
+}
