74771ee2d32c5288f7849c5010f5e7445b77b1dd
mlozeman-creator <m.lozeman@gmail.com>
2026-09-04 15:35:09 +0200

    Update API endpoint to use gemini-3.8 model

--- api/chat.js
+++ api/chat.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         console.log("VOOR FETCH");
 
         const response = await fetch(
-            `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.8-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
             {
                 method: "POST",
                 headers: {
